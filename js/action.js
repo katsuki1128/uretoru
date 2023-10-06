1c5c97cd760cc35ef6a1eb545a418acf26ce4ec8
香月 浩一 <k_katsuki@fbs.local>
2023-10-06 13:55:31 +0900

    count

--- js/action.js
+++ js/action.js
@@ -94,8 +94,10 @@ const executeActionsForStatus1 = () => {
 
     //その日に３つ送ったらスタンプがクリックできなくなる関数
     let currentCount = getCurrentCount();
+    console.log(currentCount);
+
     // console.log(currentCount);
-    if (currentCount >= 2) {
+    if (currentCount >= 3) {
         addNeverClickableClass();
     }
 
@@ -186,7 +188,7 @@ window.onload = () => {
     const date = location.search.replace("?", "");
     const checkNow = (Number(date) && date.length == 12) ? date : "";
 
-    const status = {};
+    // const status = {};
     status.checkNow = checkNow;
     const jsonString = JSON.stringify(status);
     const jsonData = JSON.parse(jsonString);
@@ -194,31 +196,31 @@ window.onload = () => {
     const stickersStatus = async (param) => {
 
         const params = { method: "post", body: JSON.stringify(param) };
-        await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickersStatus.php", params)
-            .then((response) => response.json())
-            .then((data) => {
-                const status = data[0].Code;
+        // await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickersStatus.php", params)
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         const status = data[0].Code;
 
-                // const status = 1;
+        const status = 1;
 
-                switch (status) {
-                    case 1:
-                        executeActionsForStatus1();
-                        break;
-                    case 2:
-                        executeActionsForStatus2();
-                        break;
-                    case 3:
-                        executeActionsForStatus2();
-                        break;
+        switch (status) {
+            case 1:
+                executeActionsForStatus1();
+                break;
+            case 2:
+                executeActionsForStatus2();
+                break;
+            case 3:
+                executeActionsForStatus2();
+                break;
 
-                    default:
-                        break;
-                }
+            default:
+                break;
+        }
 
-            })
-            .catch((error) => {
-            })
+        // })
+        // .catch((error) => {
+        // })
     }
     stickersStatus(jsonData);
 };
@@ -420,6 +422,7 @@ const getCurrentDate = () => {
 const getCurrentCount = () => {
     const today = getCurrentDate();
     return parseInt(localStorage.getItem(`imageClickCount_${today}`) || "0");
+    console.log()
 };
 
 const updateCount = (targetPrefix) => {
@@ -553,34 +556,34 @@ const removeClickedImageAndEnableClick = (element) => {
 //----------------------------------------
 const stickers = async (param, element) => {
     const params = { method: "post", body: JSON.stringify(param) };
-    await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickers.php", params)
-        .then((response) => response.json())
-        .then((data) => {
-            // 非同期処理が成功した場合
-            const code = data[0].Code;
+    // await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickers.php", params)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         // 非同期処理が成功した場合
+    //         const code = data[0].Code;
 
-            // code = 1;
-            switch (code) {
-                case 1:
-                    // console.log('データ登録成功');
-                    setTimeout(() => {
-                        removeClickedImageAndEnableClick(element);
-                    }, 1000);
-                    break;
-                case 2:
-                    // console.log('データ登録失敗');
-                    break;
-                case 3:
-                    // console.log('データベース接続失敗');
-                    break;
-                default:
-                    break;
-            }
-            // console.log(param)
-        })
-        .catch((error) => {
-            // 非同期処理が失敗した場合
-        })
+    code = 1;
+    switch (code) {
+        case 1:
+            // console.log('データ登録成功');
+            setTimeout(() => {
+                removeClickedImageAndEnableClick(element);
+            }, 1000);
+            break;
+        case 2:
+            // console.log('データ登録失敗');
+            break;
+        case 3:
+            // console.log('データベース接続失敗');
+            break;
+        default:
+            break;
+    }
+    // console.log(param)
+    // })
+    // .catch((error) => {
+    //     // 非同期処理が失敗した場合
+    // })
 };
 
 //----------------------------------------
