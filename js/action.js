9e8fe82601fbcdc55956a5a3952ebc676c960580
香月 浩一 <k_katsuki@fbs.local>
2023-11-02 12:57:42 +0900

    new stamp

--- js/action.js
+++ js/action.js
@@ -196,31 +196,31 @@ window.onload = () => {
     const stickersStatus = async (param) => {
 
         const params = { method: "post", body: JSON.stringify(param) };
-        // await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickersStatus.php", params)
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         const status = data[0].Code;
+        await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickersStatus.php", params)
+            .then((response) => response.json())
+            .then((data) => {
+                const status = data[0].Code;
 
-        const status = 1;
+                // const status = 1;
 
-        switch (status) {
-            case 1:
-                executeActionsForStatus1();
-                break;
-            case 2:
-                executeActionsForStatus2();
-                break;
-            case 3:
-                executeActionsForStatus2();
-                break;
+                switch (status) {
+                    case 1:
+                        executeActionsForStatus1();
+                        break;
+                    case 2:
+                        executeActionsForStatus2();
+                        break;
+                    case 3:
+                        executeActionsForStatus2();
+                        break;
 
-            default:
-                break;
-        }
+                    default:
+                        break;
+                }
 
-        // })
-        // .catch((error) => {
-        // })
+            })
+            .catch((error) => {
+            })
     }
     stickersStatus(jsonData);
 };
@@ -288,8 +288,6 @@ const adjustDottedCircleSize = () => {
 
 // ブラウザのリサイズイベントに応じて調整
 window.addEventListener('resize', adjustDottedCircleSize);
-
-
 
 
 //----------------------------------------
@@ -450,7 +448,6 @@ const removeDataOlderThan = (days) => {
     }
 };
 
-// 使用例:
 removeDataOlderThan(3);  // 3日以上前のデータを削除
 
 //----------------------------------------
@@ -558,34 +555,34 @@ const removeClickedImageAndEnableClick = (element) => {
 //----------------------------------------
 const stickers = async (param, element) => {
     const params = { method: "post", body: JSON.stringify(param) };
-    // await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickers.php", params)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         // 非同期処理が成功した場合
-    //         const code = data[0].Code;
+    await fetch("https://santaclaus.fbs.co.jp/assets/lib/MentaiStickers.php", params)
+        .then((response) => response.json())
+        .then((data) => {
+            // 非同期処理が成功した場合
+            const code = data[0].Code;
 
-    code = 1;
-    switch (code) {
-        case 1:
-            // console.log('データ登録成功');
-            setTimeout(() => {
-                removeClickedImageAndEnableClick(element);
-            }, 1000);
-            break;
-        case 2:
-            // console.log('データ登録失敗');
-            break;
-        case 3:
-            // console.log('データベース接続失敗');
-            break;
-        default:
-            break;
-    }
-    // console.log(param)
-    // })
-    // .catch((error) => {
-    //     // 非同期処理が失敗した場合
-    // })
+            // code = 1;
+            switch (code) {
+                case 1:
+                    console.log('データ登録成功');
+                    setTimeout(() => {
+                        removeClickedImageAndEnableClick(element);
+                    }, 1000);
+                    break;
+                case 2:
+                    console.log('データ登録失敗');
+                    break;
+                case 3:
+                    console.log('データベース接続失敗');
+                    break;
+                default:
+                    break;
+            }
+            // console.log(param)
+        })
+        .catch((error) => {
+            // 非同期処理が失敗した場合
+        })
 };
 
 //----------------------------------------
